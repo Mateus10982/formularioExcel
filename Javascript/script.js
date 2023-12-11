1a40fc5c5918fb370c05f1a7643e7252db0a9e53
Mateus10982 <141938661+Mateus10982@users.noreply.github.com>
2023-12-11 20:00:07 -0300

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,23 @@
-document.getElementById('form').addEventListener('submit',function(ev){
-    ev.defaultPrevented;
-})
+const fs = require('fs');
+
+// Caminho do arquivo TXT
+const arquivo = 'caminho/do/arquivo.txt';
+
+// Lê o arquivo e exibe o conteúdo
+fs.readFile(arquivo, 'utf8', function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  
+  console.log("Conteúdo do arquivo antes da adição:\n" + data);
+
+  // Adiciona uma nova linha ao final do arquivo
+  const novaLinha = "Nova linha a ser adicionada\n";
+  fs.appendFile(arquivo, novaLinha, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    
+    console.log("\nConteúdo do arquivo após a adição:\n" + fs.readFileSync(arquivo, 'utf8'));
+  });
+});
